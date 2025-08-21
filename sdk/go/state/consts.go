f4971b41b7f541ead7d5dfc0bf4ea13bc0a9a930
aereaco <admin@aerea.co>
2025-08-21 04:30:26 -0500

    updated bundles

--- sdk/go/state/consts.go
+++ sdk/go/state/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     StateKey = "state"
     Version                   = "0.0.1-alpha"
-    VersionClientByteSize     = 87526
-    VersionClientByteSizeGzip = 29468
+    VersionClientByteSize     = 88696
+    VersionClientByteSizeGzip = 29890
 
     //region Default durations
 
